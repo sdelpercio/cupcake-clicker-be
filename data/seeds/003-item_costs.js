b5f9705f76765b9cfd0451754dc1f044eb487b0f
Shawn DelPercio <sdelperc@gmail.com>
2020-09-10 16:22:41 -0400

    changed raw function in users seed

--- data/seeds/003-item_costs.js
+++ data/seeds/003-item_costs.js
@@ -1,29 +1,27 @@
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex("item_costs")
-    .truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex("item_costs").insert([
-        {
-          user_id: 1,
-          toastersCost: 20,
-          ovensCost: 100,
-          industrialOvensCost: 500,
-          friendsCost: 1000,
-          chefsCost: 5000,
-          cupcakeGodsCost: 100000,
-        },
-        {
-          user_id: 2,
-          toastersCost: 2000,
-          ovensCost: 10000,
-          industrialOvensCost: 50000,
-          friendsCost: 100000,
-          chefsCost: 500000,
-          cupcakeGodsCost: 10000000,
-        },
-        { user_id: 3 },
-      ]);
-    });
+  return knex("item_costs").then(function () {
+    // Inserts seed entries
+    return knex("item_costs").insert([
+      {
+        user_id: 1,
+        toastersCost: 20,
+        ovensCost: 100,
+        industrialOvensCost: 500,
+        friendsCost: 1000,
+        chefsCost: 5000,
+        cupcakeGodsCost: 100000,
+      },
+      {
+        user_id: 2,
+        toastersCost: 2000,
+        ovensCost: 10000,
+        industrialOvensCost: 50000,
+        friendsCost: 100000,
+        chefsCost: 500000,
+        cupcakeGodsCost: 10000000,
+      },
+      { user_id: 3 },
+    ]);
+  });
 };
